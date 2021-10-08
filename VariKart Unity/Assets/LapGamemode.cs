d9792fba813f95dfde93456965e6d86d3145c4ef
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2021-10-08 19:30:05 +1300

    Lap Gamemode v1

---
+++ VariKart Unity/Assets/LapGamemode.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LapGamemode : MonoBehaviour
+{
+    public float start = 0;
+    public float end = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(start == 4 && end == 3)
+        {
+            Debug.Log("Finished");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "start")
+        {
+            start += 1;
+        }
+        if(other.tag == "end")
+        {
+            end += 1;
+        }
+    }
+}
