9b28b142c0ff9705b3edf2b81ce15c6e57a12ff6
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2021-10-20 22:35:23 +1300

    people dancing WOOOO

--- VariKart Unity/Assets/Dancing.cs
+++ VariKart Unity/Assets/Dancing.cs
@@ -6,9 +6,7 @@ using PathCreation;
 public class Dancing : MonoBehaviour
 {
     public float speed = 5;
-    public float rotationSpeed = 5;
-    public Rigidbody rb1;
-    public Rigidbody rb2;
+    
     
 
     // Start is called before the first frame update
@@ -20,8 +18,7 @@ public class Dancing : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb1.velocity = transform.forward * speed * Time.deltaTime;
-        rb2.velocity = transform.forward * speed * Time.deltaTime;
-        transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * rotationSpeed, Space.World);
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+
     }
 }
