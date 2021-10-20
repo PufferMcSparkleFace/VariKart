d5faa7663b068efd3fc22b9e90a142289062dad4
Daniel Gutierrez <83122636+PufferMcSparkleFace@users.noreply.github.com>
2021-10-20 22:46:26 +1300

    Track Finished!

---
+++ VariKart Unity/Assets/Blade.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    public float speed = 100;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, speed * Time.deltaTime);
+
+    }
+}
